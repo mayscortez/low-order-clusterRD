09c89c5696107d6c7499bb57c50f75a51d7f582f
Mayleen Cortez <43624631+mayscortez@users.noreply.github.com>
2023-10-03 15:21:06 -0400

    created seperate file for running experiments

---
+++ Increasing Edges/execute_main.py
@@ -0,0 +1,13 @@
+# Main file for running experiments
+from main_increasing_edges import main
+import os
+
+path = os.getcwd()
+print("Path = {}".format(path))
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+beta = [1,2,3]
+graphNum = 30       
+T = 50 
+for b in beta:
+    main(b, graphNum, T)
