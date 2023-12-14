30d075b97b6caf4e755ed41b4800adcf7db44f82
Mayleen Cortez <43624631+mayscortez@users.noreply.github.com>
2023-12-14 09:31:37 -0500

    updated to run multiple

--- Covariate Community Correlation/execute_main.py
+++ Covariate Community Correlation/execute_main.py
@@ -6,8 +6,13 @@ path = os.getcwd()
 print("Path = {}".format(path))
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-beta = [1,2,3]
-graphNum = 50   
-T = 50
+beta = [1]
+B = 0.5
+#probs = [1, 5/7, 0.5, 0.3, 0.25, 0.2, 5/35, 0.1]
+#probs = [1, 25/30, 25/35, 0.625, 25/45, 0.5]
+probs = [1,B]
+graphNum = 5   
+T = 5
 for b in beta:
-    main(b, graphNum, T)
+    for p in probs:
+        main(b, graphNum, T, B, p)
