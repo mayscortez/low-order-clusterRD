1ea897acc7f9445fc89831b4fdf48570af2f5757
Mayleen Cortez <43624631+mayscortez@users.noreply.github.com>
2023-12-17 16:47:30 -0500

    improved readability, updated parameters

--- Covariate Community Correlation/execute_main.py
+++ Covariate Community Correlation/execute_main.py
@@ -1,18 +1,24 @@
 # This file is to run the experiments where we vary the correlation between treatment effect type and community type
 from main_correlation import main
+import numpy as np
 import os
 
 path = os.getcwd()
 print("Path = {}".format(path))
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-beta = [1]
-B = 0.5
-#probs = [1, 5/7, 0.5, 0.3, 0.25, 0.2, 5/35, 0.1]
-#probs = [1, 25/30, 25/35, 0.625, 25/45, 0.5]
-probs = [1,B]
-graphNum = 5   
-T = 5
-for b in beta:
-    for p in probs:
-        main(b, graphNum, T, B, p)
+beta = [1,2,3]
+B = [0.06, 0.5, 0.5]
+probs = [[0.06, 0.25, 1/3, 2/3, 1],     # K in [50, 12, 9, 6, 3]
+         [0.5, 0.625, 25/33, 25/29, 1], # K in [50, 40, 33, 29, 25]
+         [0.5, 0.625, 25/33, 25/29, 1]]
+graphNum = 50   
+T = 50
+for b in range(len(beta)):
+    print('=====================================')
+    print('Experiments for degree: {}'.format(b+1))
+    print('=====================================')
+    for p in probs[b]:
+        print('B = {}, p = {}'.format(B[b], p))
+        print()
+        main(beta[b], graphNum, T, B[b], p)
