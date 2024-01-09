11c5d9e1dac4682234c918546c9c652a7d7e0ff7
Mayleen Cortez <43624631+mayscortez@users.noreply.github.com>
2024-01-09 14:00:57 -0500

    updated polynomial interpolation estimator
    returns 0 if no clusters are chosen (n=0, essentially) to avoid divide by zero error

--- Covariate Community Correlation/experiment_functions.py
+++ Covariate Community Correlation/experiment_functions.py
@@ -408,7 +408,10 @@ def graph_agnostic(n, sums, H):
     H (numpy array): PPOM coefficients h_t or l_t
     sums (numpy array): sums of outcomes at each time step
     '''
-    return (1/n)*H.dot(sums)
+    if n > 0:
+        return (1/n)*H.dot(sums)
+    else:
+        return 0
 
 def poly_interp_splines(n, P, sums, spltyp = 'quadratic'):
   '''
