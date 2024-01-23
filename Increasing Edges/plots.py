d5866703693d0bf00842454e69d133dd8a4a24d0
Mayleen Cortez <43624631+mayscortez@users.noreply.github.com>
2024-01-23 10:45:58 -0500

    updated to run multiple experiments

--- Increasing Edges/plots.py
+++ Increasing Edges/plots.py
@@ -5,48 +5,46 @@ import pandas as pd
 import seaborn as sns
 import os
 
-def main():
-    graph = "SBM" 
+def main(beta=[1,2,3], B=0.06, p=1, estimators = ['PI-$n$($p$)', 'HT', 'LS-Prop', 'DM','DM($0.75$)']):
     n = 1000
     nc = 50
-    q = 0.06 
-    B = 0.06
-    K = int(np.floor(q*nc))
-    p = B/q
-    #q_or_K_st = '_K' + str(K)
-    Pii = 10/(n/nc) 
     
     fixed =  '_n' + str(n) + '_nc' + str(nc) + '_' + 'B' + str(B).replace('.','') + '_p' + str(np.round(p,3)).replace('.','')  # naming convention
-    x_label = [fixed + "_incrEdges", fixed + "_incrEdges", fixed + "_incrEdges"]
+    x_label = "incrEdges" + fixed
     x_var = ['Pij', 'global', 'average']
-    x_plot = ['Probability of an edge between 2 communities $P_{out}$',
-              'Global clustering coefficient $C$',
-              'Network Average Clustering Coefficient $C$']
-    beta = [1,2,3]
+    x_plot = ['Probability of an edge between 2 communities $p_{out}$',
+              'Global clustering coefficient $\mathcal{C}_\mathrm{global}$',
+              'Network Average Clustering Coefficient $\mathcal{C}_\mathrm{avg}$']
     for b in beta:
-        title = ['$\\beta={}, n={}, B={}, n_c={}, K={}, E[d_i]=10$'.format(b, n, B, nc, K),
-                 '$\\beta={}, n={}, B={}, n_c={}, K={}, E[d_i]=10$'.format(b, n, B, nc, K),
-                 '$\\beta={}, n={}, B={}, n_c={}, K={}, E[d_i]=10$'.format(b, n, B, nc, K)]
+        title = ['$\\beta={}, n={}, n_c={}, B={}, p={}, E[d_i]=10$'.format(b, n, nc, B, p),
+                 '$\\beta={}, n={}, n_c={}, B={}, p={}, E[d_i]=10$'.format(b, n, nc, B, p),
+                 '$\\beta={}, n={}, n_c={}, B={}, p={}, E[d_i]=10$'.format(b, n, nc, B, p)]
         for ind in range(len(x_var)):
-            plot(graph,x_var[ind],x_label[ind],b,x_plot[ind],title[ind],b)
+            plot(x_var[ind],x_label,b,x_plot[ind],title[ind],estimators)
 
 
-def plot(graph,x_var,x_label,model,x_plot,title,beta=1):
+def plot(x_var,x_label,beta,x_plot,title,estimators):
     load_path = 'output/' + 'deg' + str(beta) + '/'
     save_path = 'plots/' + 'deg' + str(beta) + '/'
     deg_str = '_deg' + str(beta)
     
-    #estimators = ['PI-$n$($p$)', 'PI-$\mathcal{U}$($p$)', 'LS-Prop', 'LS-Num','DM', 'DM($0.75$)']
-    #estimators = ['PI-$n$($p$)'] 
-    #estimators = ['PI-$\mathcal{U}$($p$)'] 
-    estimators = ['PI-$n$($p$)', 'PI-$\mathcal{U}$($p$)'] 
-    estimators_str = '_nU' # other options: 'n', 'U', 'all' -- basically, which estimators show up in the plots
+    color_map = {'PI-$n$($p$)': '#2596be', 
+                 'PI-$\mathcal{U}$($p$)': '#1b45a6',
+                 'HT': '#e51e31', 
+                 'DM-C': '#fb5082', 
+                 'DM-C($0.75$)': '#ff7787',
+                 'PI-$n$($B$)': '#ff7f0e',
+                 'LS-Prop': '#2ca02c',
+                 'LS-Num': '#9467bd',
+                 'DM': '#2ca02c',
+                 'DM($0.75$)':'#7f7f7f'}
+    color_pal = [color_map[est] for est in estimators]
 
     experiment = x_label
-    print(experiment+deg_str+'_'+x_var+estimators_str)
+    print(experiment+deg_str+'_'+x_var)
 
     # Create and save plots
-    df = pd.read_csv(load_path+graph+experiment+'-full-data' + deg_str+ '.csv')
+    df = pd.read_csv(load_path + experiment + deg_str + '_bernoulli-full.csv')
     newData = df.loc[df['Estimator'].isin(estimators)]
 
     plt.rc('text', usetex=True)
@@ -55,7 +53,7 @@ def plot(graph,x_var,x_label,model,x_plot,title,beta=1):
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
-    sns.lineplot(x=x_var, y='Bias', hue='Estimator', style='Estimator', data=newData, errorbar='sd', legend='brief', markers=True)
+    sns.lineplot(x=x_var, y='Bias', hue='Estimator', style='Estimator', data=newData, errorbar='sd', legend='brief', markers=True, palette=color_pal)
 
     #ax.set_xlim(0,0.001)
     ax.set_xlabel(x_plot, fontsize = 18)
@@ -67,16 +65,17 @@ def plot(graph,x_var,x_label,model,x_plot,title,beta=1):
     plt.grid()
 
     plt.tight_layout()
-    plt.savefig(save_path+graph+'_'+x_var+deg_str+experiment+estimators_str+'.pdf')
+    plt.savefig(save_path + experiment + deg_str + '_' + x_var + '_bern.png')
     plt.close()
 
     #Create and save MSE plots
     fig2 = plt.figure()
     ax2 = fig2.add_subplot(111)
-    newData['RelBias_sq'] = df['Bias']**2
-    sns.lineplot(x=x_var, y='RelBias_sq', hue='Estimator', style='Estimator', data=newData, legend='brief', markers=True)
+    sns.lineplot(x=x_var, y='Rel_bias_sq', hue='Estimator', style='Estimator', data=newData, errorbar=None, legend='brief', markers=True, palette=color_pal)
 
+    #ax.set_xlim(0,0.001)
     ax2.set_xlabel(x_plot, fontsize = 18)
+    #ax.set_ylim(-0.75,0.75)
     ax2.set_ylabel("MSE", fontsize = 18)
     ax2.set_title(title, fontsize=20)
     handles, labels = ax2.get_legend_handles_labels()
@@ -84,10 +83,27 @@ def plot(graph,x_var,x_label,model,x_plot,title,beta=1):
     plt.grid()
     plt.tight_layout()
 
-    plt.savefig(save_path+graph+'_'+x_var+deg_str+experiment+estimators_str+'_MSE.pdf')
+    plt.savefig(save_path + experiment + deg_str + '_' + x_var +'_bern_MSE.png')
     plt.close()
 
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    main()
+
+    ''' 
+    beta = [1,2,3]
+    B = 0.06
+    probs = [0.06, 0.25, 1/3, 2/3, 1]    # K in [50, 12, 9, 6, 3] 
+    '''
+    beta = [1,2,3]
+    B = 0.06
+    probs = [1]
+    
+    # All possible estimators: ['PI-$n$($p$)', 'PI-$\mathcal{U}$($p$)', 'HT', 'DM-C', 'DM-C($0.75$)', 'PI-$n$($B$)', 'LS-Prop', 'LS-Num','DM', 'DM($0.75$)']
+    # Note: for colors to match in each plot, the estimator names should be in the same relative order as above
+    estimators = ['PI-$\mathcal{U}$($p$)', 'HT','DM-C', 'DM-C($0.75$)', 'PI-$n$($B$)']
+
+    for p in probs:
+        print('Plotting p: {} (bernoulli design)'.format(p))
+        print()
+        main(beta, B, p, estimators)
