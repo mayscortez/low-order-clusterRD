f471c2272b596a8a2aa0078598a96a32d4d5f671
Mayleen Cortez <43624631+mayscortez@users.noreply.github.com>
2024-02-14 10:14:22 -0500

    updated naming conventions

--- Covariate Community Correlation/main_correlation.py
+++ Covariate Community Correlation/main_correlation.py
@@ -8,17 +8,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import random
 import time
-from experiment_functions import *
-import pickle
-import sys
+from experiment_functions import PI, horvitz_thompson, DM_naive, DM_fraction, poly_LS_prop, poly_LS_num, SBM, binary_covariate_weights, ppom, seq_treatment_probs, bern_coeffs, select_clusters_complete, select_clusters_bernoulli, staggered_rollout_bern_clusters, outcome_sums, staggered_rollout_bern
 
-def main(beta, graphNum, T, B=0.06, p=1, p_in=0.5, cluster_selection_RD = "bernoulli"):
+
+def main(beta, graphNum, T, U, p=0.06, q=1, p_in=0.5, cluster_selection_RD = "bernoulli"):
     '''
     beta (int): degree of the model
     graphNum (int): how many graph models to average over
     T (int): how many treatment samples to average over
-    B (float): treatment budget (marginal treatment probability)
-    p (float): treatment probability if cluster is chosen (treatment prob conditioned on being in U)
+    p (float): treatment budget (marginal treatment probability)
+    q (float): treatment probability if cluster is chosen (treatment prob conditioned on being in U)
     cluster_selection_RD (str): either "complete" or "bernoulli" depending on the design used for selecting clusters
     '''
     deg_str = '_deg' + str(beta)  # naming convention
@@ -30,10 +29,9 @@ def main(beta, graphNum, T, B=0.06, p=1, p_in=0.5, cluster_selection_RD = "berno
     nc = 50                   # number of communities
     p_out = (0.5-p_in)/49     # edge probability between different communities, this way keeps expected degree constant at 10
 
-    K_expected = int(B * nc / p)   # if complete design, this is actual number of chosen clusters; if Bernoulli this is expected number of chosen clusters
-    q_expected = K_expected/nc 
+    K_expected = int(p * nc / q)   # if complete design, this is actual number of chosen clusters; if Bernoulli this is expected number of chosen clusters
     
-    fixed = '_n' + str(n) + '_nc' + str(nc) + '_' + 'in' + str(np.round(p_in,3)).replace('.','') + '_out' + str(np.round(p_out,3)).replace('.','') + '_p' + str(np.round(p,3)).replace('.','') + '_B' + str(B).replace('.','') # naming convention
+    fixed = '_n' + str(n) + '_nc' + str(nc) + '_' + 'in' + str(np.round(p_in,3)).replace('.','') + '_out' + str(np.round(p_out,3)).replace('.','') + '_q' + str(np.round(q,3)).replace('.','') + '_p' + str(p).replace('.','') # naming convention
     
     f = open(save_path + experiment + fixed + deg_str + '_' + cluster_selection_RD + '.txt', 'w') # e.g filename could be correlation_n1000_nc50_in005_out0_p1_B01_deg2_bernoulli.txt 
     startTime1 = time.time()
@@ -51,7 +49,7 @@ def main(beta, graphNum, T, B=0.06, p=1, p_in=0.5, cluster_selection_RD = "berno
         print("phi = {}".format(phi))
         startTime2 = time.time()
 
-        results.extend(run_experiment(beta, n, nc, B, p, r, diag, p_in, p_out, phi, cluster_selection_RD, q_expected, K_expected, graphNum, T, graphStr))
+        results.extend(run_experiment(beta, n, nc, p, q, r, diag, p_in, p_out, phi, cluster_selection_RD, K_expected, graphNum, T, U, graphStr))
 
         executionTime = (time.time() - startTime2)
         print('Runtime (in seconds) for phi = {} step: {}'.format(phi, executionTime),file=f)
@@ -66,20 +64,19 @@ def main(beta, graphNum, T, B=0.06, p=1, p_in=0.5, cluster_selection_RD = "berno
     df.to_csv(save_path + experiment + fixed + deg_str + '_' + cluster_selection_RD + '-full.csv') # e.g filename could be correlation_n1000_nc50_in005_out0_p1_B01_deg2_bernoulli-full.csv 
     
 
-def run_experiment(beta, n, nc, B, target_p, r, diag, Pii, Pij, phi, design, Eq, EK, graphNum, T, graphStr, p_prime=0, realized = True):
+def run_experiment(beta, n, nc, p, q, r, diag, Pii, Pij, phi, design, EK, graphNum, T, U, graphStr, p_prime=0, realized = True):
     '''
     beta = degree of the model / polynomial
     n = population size
     nc = number of clusters
-    B = original treatment budget/fraction
-    target_p = the treatment probability within clusters i.e. the target treatment probability - note this may differ from the realized t.p.
+    p = original treatment budget/fraction
+    q = the treatment probability within clusters i.e. the target treatment probability - note this may differ from the realized t.p.
     r = ratio offdiag/diag: (indirect effect)/(direct effects)
     diag = maxium norm of the direct effects before covariate type scaling
     Pii = edge probability within communities
     Pij = edge prob btwn different communities
     phi = correlation btwn community & effect type (probability between 0 and 0.5)
     design = design being used for selecting clusters, either "complete" or "bernoulli"
-    Eq = expected value of q E[K/nc] (i.e. expected fraction of clusters that are chosen)
     EK = expected value of K E[K] (i.e. expected number of chosen clusters)
     graphNum = number of graphs to average over
     T = number of trials per graph
@@ -90,17 +87,17 @@ def run_experiment(beta, n, nc, B, target_p, r, diag, Pii, Pij, phi, design, Eq,
     
     offdiag = r*diag   # maximum norm of indirect effect
 
-    dict_base = {'n': n, 'nc': nc, 'Pii': Pii, 'Pij': Pij, 'Phi': phi, 'B': B, 'p': target_p, 'EK': EK, 'Eq': Eq, 'ratio': r}
+    dict_base = {'n': n, 'nc': nc, 'Pii': Pii, 'Pij': np.round(Pij,3), 'Phi': phi, 'p': p, 'q': q, 'EK': EK, 'ratio': r}
 
     # Cluster Randomized Design Estimators
     estimators_clusterRD = []
-    estimators_clusterRD.append(lambda q,y,z,sums,H_m,sums_U: PI(n*q,sums,H_m))             # estimator looks at all [n]
-    estimators_clusterRD.append(lambda q,y,z,sums,H_m,sums_U: PI(n*q,sums_U,H_m))           # estimator only looking at [U]
-    estimators_clusterRD.append(lambda q,y,z,sums,H_m,sums_U: horvitz_thompson(n, nc, y, A, z, Eq, target_p))  
-    estimators_clusterRD.append(lambda q,y,z,sums,H_m,sums_U: DM_naive(y,z))                 # difference in means 
-    estimators_clusterRD.append(lambda q,y,z,sums,H_m,sums_U: DM_fraction(n,y,A,z,0.75))     # thresholded difference in means
+    estimators_clusterRD.append(lambda x,y,z,sums,H_m,sums_U: PI(n*x,sums,H_m))             # estimator looks at all [n]
+    estimators_clusterRD.append(lambda x,y,z,sums,H_m,sums_U: PI(n*x,sums_U,H_m))           # estimator only looking at [U]
+    estimators_clusterRD.append(lambda x,y,z,sums,H_m,sums_U: horvitz_thompson(n, nc, y, A, z, EK/nc, q))  
+    estimators_clusterRD.append(lambda x,y,z,sums,H_m,sums_U: DM_naive(y,z))                 # difference in means 
+    estimators_clusterRD.append(lambda x,y,z,sums,H_m,sums_U: DM_fraction(n,y,A,z,0.75))     # thresholded difference in means
 
-    alg_names_clusterRD = ['PI-$n$($p$)', 'PI-$\mathcal{U}$($p$)', 'HT', 'DM-C', 'DM-C($0.75$)']
+    alg_names_clusterRD = ['PI($q$)', 'PI-$\mathcal{U}$($p$)', 'HT', 'DM-C', 'DM-C($0.75$)']
 
     # Bernoulli Randomized Design Estimators
     estimators_bernRD = []
@@ -110,7 +107,7 @@ def run_experiment(beta, n, nc, B, target_p, r, diag, Pii, Pij, phi, design, Eq,
     estimators_bernRD.append(lambda y,z,sums,H_m: DM_naive(y,z))                 # difference in means 
     estimators_bernRD.append(lambda y,z,sums,H_m: DM_fraction(n,y,A,z,0.75))     # thresholded difference in means
 
-    alg_names_bernRD = ['PI-$n$($B$)', 'LS-Prop', 'LS-Num','DM', 'DM($0.75$)']
+    alg_names_bernRD = ['PI($p$)', 'LS-Prop', 'LS-Num','DM', 'DM($0.75$)']
 
     results = []
     for g in range(graphNum):
@@ -131,53 +128,60 @@ def run_experiment(beta, n, nc, B, target_p, r, diag, Pii, Pij, phi, design, Eq,
 
         # compute the true TTE
         TTE = 1/n * np.sum((fy(np.ones(n)) - fy(np.zeros(n))))
+        dict_base.update({'TTE': np.round(TTE,5)})
         
         ####### Estimate ########
 
         # parameters for the staggered rollout - Cluster Randomized Design
-        P = seq_treatment_probs(beta, target_p)        # treatment probabilities for each step of the staggered rollout on U
+        P = seq_treatment_probs(beta, q)        # treatment probabilities for each step of the staggered rollout on U
         P_prime = seq_treatment_probs(beta, p_prime)  # treatment probabilities for each step of the staggered rollout on the boundary of U
         H = bern_coeffs(P)                      # coefficients for the polynomial interpolation estimator
 
         # parameters for the staggered rollout - Bernoulli Randomized Design
-        P_bernRD = seq_treatment_probs(beta, B) # treatment probabilities for each step of the staggered rollout on [n]
+        P_bernRD = seq_treatment_probs(beta, p) # treatment probabilities for each step of the staggered rollout on [n]
         H_bern = bern_coeffs(P_bernRD)          # coefficients for the polynomial interpolation estimator
 
-        for i in range(T):
+        for i in range(U):
             # select clusters 
             if design == "complete":
                 selected = select_clusters_complete(nc, EK)
                 K_real = EK
-                q_real = Eq
             else:
-                selected = select_clusters_bernoulli(nc, Eq)
+                selected = select_clusters_bernoulli(nc, EK/nc)
                 K_real = len(selected)
-                q_real = K_real/nc
 
-            #print("Design: {}, # of chosen clusters: {}".format(design, len(selected)))
             selected_nodes = [x for x,y in G.nodes(data=True) if (y['block'] in selected)] # get the nodes in selected clusters
 
-            dict_base.update({'rep': i, 'design': 'Cluster', 'K': K_real, 'q': q_real})
+            dict_base.update({'rep_U': i, 'design': 'Cluster', 'K': K_real})
 
-            # Cluster Randomized Design
-            Z = staggered_rollout_bern_clusters(n, selected_nodes, P, [], P_prime)
-            z = Z[beta,:]
-            y = fy(z)
-            sums, sums_U = outcome_sums(n, fy, Z, selected_nodes) # the sums corresponding to all nodes (i.e. [n]) and just selected nodes (i.e. [U])
+            EZ = np.zeros(shape=(beta+1,n))
+            for b in range(1,beta+1):
+                EZ[b,selected_nodes] = P[b]
 
-            for x in range(len(estimators_clusterRD)):
-                if realized:
-                    bias_correction = 1 - (1-Eq)**nc
-                    est = estimators_clusterRD[x](q_real*bias_correction,y,z,sums,H,sums_U)
-                else:
-                    est = estimators_clusterRD[x](Eq,y,z,sums,H,sums_U)
-                dict_base.update({'Estimator': alg_names_clusterRD[x], 'Bias': (est-TTE)/TTE, 'Abs_Bias': (est-TTE), 'Rel_bias_sq':((est-TTE)/TTE)**2, 'Bias_sq': ((est-TTE)**2)})
-                results.append(dict_base.copy())
-                # Testing for edge case
-                if isinstance(est, list):
-                    with open('error_variables.picle', 'wb') as f:
-                        pickle.dump({'A': A, 'P': P, 'H': H, 'selected': selected, 'U': selected_nodes, 'Z': Z, 'sums': sums, 'sums_U': sums_U, 'estimator': alg_names_clusterRD[x], 'estimate': est}, f)
-                    raise ValueError('The estimator {} returned an invalid value {}'.format(alg_names_clusterRD[x], est))
+            Esums = outcome_sums(n, fy, EZ, selected_nodes)[0]
+
+            bias_correction = 1 - (1-(EK/nc))**nc
+            conditional_E = PI(n*(K_real/nc)*bias_correction, Esums, H)
+
+            dict_base.update({'Estimator': 'E[PI$(q)|\mathcal{U}$]', 'est': np.round(conditional_E,5), 'Bias': (conditional_E-TTE)/TTE, 'Abs_Bias': (conditional_E-TTE), 'Rel_bias_sq':((conditional_E-TTE)/TTE)**2, 'Bias_sq': ((conditional_E-TTE)**2)})
+            results.append(dict_base.copy())
+
+            for j in range(T):
+                dict_base.update({'rep_z': j})
+                # Cluster Randomized Design
+                Z = staggered_rollout_bern_clusters(n, selected_nodes, P, [], P_prime)
+                z = Z[beta,:]
+                y = fy(z)
+                sums, sums_U = outcome_sums(n, fy, Z, selected_nodes) # the sums corresponding to all nodes (i.e. [n]) and just selected nodes (i.e. [U])
+
+                for x in range(len(estimators_clusterRD)):
+                    if realized:
+                        bias_correction = 1 - (1-(EK/nc))**nc
+                        est = estimators_clusterRD[x]((K_real/nc)*bias_correction,y,z,sums,H,sums_U)
+                    else:
+                        est = estimators_clusterRD[x](EK/nc,y,z,sums,H,sums_U)
+                    dict_base.update({'Estimator': alg_names_clusterRD[x], 'Bias': (est-TTE)/TTE, 'Abs_Bias': (est-TTE), 'Rel_bias_sq':((est-TTE)/TTE)**2, 'Bias_sq': ((est-TTE)**2)})
+                    results.append(dict_base.copy())
 
 
             # Bernoulli Randomized Design (No Clusters)
@@ -223,81 +227,4 @@ def covariate_type(i, n, K, num=2):
     div = n // num
     return i // div
 
-def covariate_weights_binary(C, minimal = 1/4, extreme = 4, phi=0):
-    '''
-    Returns a weighted adjacency matrix where weights are determined by covariate type. We assume a binary effect types covariate
-
-    C (array): weights without effect type covariate
-    minimal (float): 
-    extreme (int):
-    phi: probability that an individual's covariate type flips
-    '''
-    n = C.shape[0]
-    scaling1 = np.zeros(n//2) + minimal
-    scaling2 = np.zeros(n//2) + extreme
-
-    R1 = np.random.rand(n//2)
-    R2 = np.random.rand(n//2)
-    R1 = (R1 < phi) + 0
-    R2 = (R2 < phi) + 0
-
-    scaling1[np.nonzero(R1)] = extreme
-    scaling2[np.nonzero(R2)] = minimal
-
-    scaling = np.concatenate((scaling1,scaling2))
-    return C.multiply(scaling)
-
-def color_nodes(n, K):
-    '''
-    Returns random colors for each of the K communities in a SBM
-
-    # https://stackoverflow.com/questions/28999287/generate-random-colors-rgb
-    int n = number of nodes
-    int K = number of communities (assumed to be of equal size, with n divisible  by K)
-    '''
-    number_of_colors = K
-    color = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)]) for i in range(number_of_colors)]
-    rep = int(n/K)
-    res = np.repeat(color, rep)
-    return res
         
-def plot_degrees(G, Pii, Pij):
-    '''
-    Saves a degree histogram for a networkx SBM. 
-
-    G (networkx graph object): the SBM
-    Pii (float): probability of edge between two nodes of the same community
-    Pij (float): probabiility of an edge between two nodes of different communities
-    '''
-    degree_sequence = sorted((d for n, d in G.degree()), reverse=True)
-    fig, ax2 = plt.subplots()
-    ax2.bar(*np.unique(degree_sequence, return_counts=True))
-    ax2.set_title("Degree histogram")
-    ax2.set_xlabel("Degree")
-    ax2.set_ylabel("# of Nodes")
-
-    fig.tight_layout()
-    Pin = str(Pii).replace('.','')
-    Pout = str(Pij).replace('.','')
-    plt.savefig('testing-graphs/degrees/degrees-' + 'Pin' + Pin + '-Pout' + Pout + '.png')
-    plt.close()
-
-def draw_SBM(n, K, G, Pii, Pij):
-    '''
-    Saves a drawing of a networkx SBM.
-
-    n (int): number of nodes
-    K (int): number of communities
-    G (networkx graph object): SBM
-    Pii (float): probability of edge between two nodes of the same community
-    Pij (float): probabiility of an edge between two nodes of different communities
-    '''
-    fig2, ax1 = plt.subplots()
-    colors = color_nodes(n,K)
-    nx.draw_networkx(G, ax=ax1, with_labels=False, node_size=10, node_color=colors,width=0.4)
-    ax1.set_title("SBM with $P_{{{}}}={}$ and $P_{{{}}}={}$".format('in', Pii, 'out', Pij))
-    fig2.tight_layout()
-    Pin = str(Pii).replace('.','')
-    Pout = str(Pij).replace('.','')
-    plt.savefig('testing-graphs/structure/structure-' + 'Pin' + Pin + '-Pout' + Pout + '.png')
-    pass
--- Covariate Community Correlation/plots.py
+++ Covariate Community Correlation/plots.py
@@ -12,40 +12,42 @@ plt.rcParams.update({
 })
 plt.rcParams["mathtext.fontset"]
 
-def main(beta=1, B=0.06, p=1, p_in = 0.5, cluster_selection = "bernoulli", estimators = ['PI-$n$($p$)', 'HT', 'LS-Prop', 'DM','DM($0.75$)'] ): 
+def main(beta=1, p=0.06, q=1, p_in = 0.5, cluster_selection = "bernoulli", estimators = ['PI-$n$($p$)', 'HT', 'LS-Prop', 'DM','DM($0.75$)'], plot_type="MSE"): 
     n = 1000
     nc = 50
     p_out = (0.5-p_in)/49
 
     load_path = 'output/' + 'deg' + str(beta) + '/' + cluster_selection + '/'
 
-    Bstr = str(np.round(B,3)).replace('.','')
     pstr = str(np.round(p,3)).replace('.','')
+    qstr = str(np.round(q,3)).replace('.','')
     
     experiment = 'correlation'
     
-    fixed = '_n' + str(n) + '_nc' + str(nc) + '_' + 'in' + str(np.round(p_in,3)).replace('.','') + '_out' + str(np.round(p_out,3)).replace('.','') + '_p' + pstr + '_B' + Bstr # naming convention
+    fixed = '_n' + str(n) + '_nc' + str(nc) + '_' + 'in' + str(np.round(p_in,3)).replace('.','') + '_out' + str(np.round(p_out,3)).replace('.','') + '_q' + qstr + '_p' + pstr # naming convention
     x_label = [experiment + fixed]
     x_var = ['Phi']
     x_plot = ['covariate balance $\phi$']
-    title = ['$\\beta={},$ SBM$({},{},{},{}), B={}, p={}$'.format(beta, n, nc, p_in, np.round(p_out,3), B, np.round(p,3))]
+    title = ['$\\beta={}, q = {}$'.format(beta, q)]
     for ind in range(len(x_var)):
-        plot(load_path,cluster_selection,x_var[ind],x_label[ind],beta,x_plot[ind],title[ind],estimators)
+        plot(load_path,cluster_selection,x_var[ind],x_label[ind],beta,x_plot[ind],title[ind],estimators,plot_type)
 
-def plot(load_path,cluster_selection_RD,x_var,x_label,model,x_plot,title,estimators):
+def plot(load_path,cluster_selection_RD,x_var,x_label,model,x_plot,title,estimators,plot_type):
     save_path = 'plots/' + 'deg' + str(model) + '/' + cluster_selection_RD + '/'
     deg_str = '_deg' + str(model)
     
-    color_map = {'PI-$n$($p$)': '#2596be', 
-                 'PI-$\mathcal{U}$($p$)': '#1b45a6',
-                 'HT': '#e51e31', 
-                 'DM-C': '#fb5082', 
-                 'DM-C($0.75$)': '#ff7787',
-                 'PI-$n$($B$)': '#ff7f0e',
-                 'LS-Prop': '#2ca02c',
-                 'LS-Num': '#9467bd',
-                 'DM': '#2ca02c',
-                 'DM($0.75$)':'#7f7f7f'}
+    color_map = {'PI($q$)': '#0296fb', 
+                 'PI-$\mathcal{U}$($q$)': '#0d7901',
+                 'HT': '#1100ff', 
+                 'DM-C': '#990002', 
+                 'DM-C($0.75$)': '#5f0099',
+                 'E[PI$(q)|\mathcal{U}$]': '#e20287', #e20287
+                 'E[PI-$\mathcal{U}(q)|\mathcal{U}$]': '#ffc900',
+                 'PI($p$)': '#0296fb',
+                 'LS-Prop': '#fb6702',
+                 'LS-Num': '#15c902',
+                 'DM': '#ff0004',
+                 'DM($0.75$)':'#9e00ff'}
     color_pal = [color_map[est] for est in estimators]
     
     experiment = x_label
@@ -57,42 +59,45 @@ def plot(load_path,cluster_selection_RD,x_var,x_label,model,x_plot,title,estimat
     newData = df.loc[df['Estimator'].isin(estimators)]
 
     plt.rc('text', usetex=True)
-    
-    # Plot with all the estimators
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
 
-    sns.lineplot(x=x_var, y='Bias', hue='Estimator', style='Estimator', data=newData, errorbar='sd', legend='brief', markers=True, palette=color_pal)
 
-    #ax.set_xlim(0,0.001)
-    ax.set_xlabel(x_plot, fontsize = 18)
-    #ax.set_ylim(0,1)
-    ax.set_ylabel("Relative Bias", fontsize = 18)
-    ax.set_title(title, fontsize=18)
-    handles, labels = ax.get_legend_handles_labels()
-    ax.legend(handles=handles, labels=labels, loc='upper right', fontsize = 14)
-    plt.grid()
-    plt.tight_layout()
+    if (plot_type == 'Bias' or plot_type == 'both'):
+        # Plot with all the estimators
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
 
-    plt.savefig(save_path + experiment + deg_str + '_' + cluster_selection_RD + '.png')
-    plt.close()
+        sns.lineplot(x=x_var, y='Bias', hue='Estimator', style='Estimator', data=newData, errorbar='sd', legend='brief', markers=True, palette=color_pal)
 
-    # Create and save MSE plots
-    fig2 = plt.figure()
-    ax2 = fig2.add_subplot(111)
-    sns.lineplot(x=x_var, y='Rel_bias_sq', hue='Estimator', style='Estimator', data=newData, errorbar=None, legend='brief', markers=True, palette=color_pal)
+        #ax.set_xlim(0,0.001)
+        ax.set_xlabel(x_plot, fontsize = 18)
+        #ax.set_ylim(0,1)
+        ax.set_ylabel("Relative Bias", fontsize = 18)
+        ax.set_title(title, fontsize=18)
+        handles, labels = ax.get_legend_handles_labels()
+        ax.legend(handles=handles, labels=labels, loc='upper right', fontsize = 14)
+        plt.grid()
+        plt.tight_layout()
 
-    ax2.set_ylim(0,1)
-    ax2.set_xlabel(x_plot, fontsize = 18)
-    ax2.set_ylabel("MSE", fontsize = 18)
-    ax2.set_title(title, fontsize=20)
-    handles, labels = ax2.get_legend_handles_labels()
-    ax2.legend(handles=handles, labels=labels, loc='upper left', fontsize = 14)
-    plt.grid()
-    plt.tight_layout()
+        plt.savefig(save_path + experiment + deg_str + '_' + cluster_selection_RD + '.png')
+        plt.close()
 
-    plt.savefig(save_path + experiment + deg_str + '_' + cluster_selection_RD + '_MSE.png')
-    plt.close()
+    if (plot_type == 'MSE' or plot_type == 'both'):
+        # Create and save MSE plots
+        fig2 = plt.figure()
+        ax2 = fig2.add_subplot(111)
+        sns.lineplot(x=x_var, y='Rel_bias_sq', hue='Estimator', style='Estimator', data=newData, errorbar=None, legend='brief', markers=True, palette=color_pal)
+
+        ax2.set_ylim(0,1)
+        ax2.set_xlabel(x_plot, fontsize = 18)
+        ax2.set_ylabel("MSE", fontsize = 18)
+        ax2.set_title(title, fontsize=20)
+        handles, labels = ax2.get_legend_handles_labels()
+        ax2.legend(handles=handles, labels=labels, loc='upper left', fontsize = 14)
+        plt.grid()
+        plt.tight_layout()
+
+        plt.savefig(save_path + experiment + deg_str + '_' + cluster_selection_RD + '_MSE.png')
+        plt.close()
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -103,18 +108,20 @@ if __name__ == "__main__":
             [0.5, 0.625, 25/33, 25/29, 1]  # K in [50, 40, 33, 29, 25] for B = 0.5
             [0.02, 0.1, 0.2, 0.3, 1]       # K in [1, 5, 10, 15, 50] for B = 0.02
     '''
-    beta = [1,2]
-    B = 0.06 
-    probs = [0.06, 0.25, 1/3, 2/3, 1]
-    p_in = 0.5
+    beta = [3]
+    budget = 0.06 
+    probs = [0.25, 3/5, 1]
+    p_in = 0.4
     design = "bernoulli"  # bernoulli   complete
     
-    # All possible estimators: ['PI-$n$($p$)', 'PI-$\mathcal{U}$($p$)', 'HT', 'DM-C', 'DM-C($0.75$)', 'PI-$n$($B$)', 'LS-Prop', 'LS-Num','DM', 'DM($0.75$)']
+    # All possible estimators: [ 'E[PI$(q)|\mathcal{U}$]', 'PI($q$)', 'PI-$\mathcal{U}$($q$)', 'HT', 'DM-C', 'DM-C($0.75$)', 'PI($p$)', 'LS-Prop', 'LS-Num','DM', 'DM($0.75$)']
     # Note: for colors to match in each plot, the estimator names should be in the same relative order as above
-    estimators = ['PI-$n$($p$)', 'PI-$\mathcal{U}$($p$)', 'HT', 'DM-C', 'DM-C($0.75$)', 'PI-$n$($B$)', 'LS-Prop', 'LS-Num']
-    
+    estimators = ['E[PI$(q)|\mathcal{U}$]', 'PI($q$)']
+
+    plot_type = "Bias"  #option: MSE, Bias, both
+
     for b in range(len(beta)):
         print('Plotting degree: {} ({} design)'.format(beta[b], design))
-        for p in probs:
+        for q in probs:
             print()
-            main(beta[b], B, p, p_in, design, estimators)
+            main(beta[b], budget, q, p_in, design, estimators, plot_type)
