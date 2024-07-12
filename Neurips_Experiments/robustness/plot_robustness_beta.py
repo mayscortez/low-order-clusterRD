106b5cba006ecc7f625a317bd1bc8c628217b371
mayscortez <mec383@cornell.edu>
2024-07-12 17:43:49 -0400

    added more stuff to plots; added if __name__ == '__main__'

--- Neurips_Experiments/robustness/plot_robustness_beta.py
+++ Neurips_Experiments/robustness/plot_robustness_beta.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pickle
+import pandas as pd
 
 def plot_robustness(df, col_var, row_var, model, x_var, plot_type="bias", save_path=""):  
     '''
@@ -18,17 +20,22 @@ def plot_robustness(df, col_var, row_var, model, x_var, plot_type="bias", save_p
     nrow = len(rows)
 
     f,ax = plt.subplots(nrow,ncol, sharex=True, sharey=True)
-    plt.setp(ax, xlim=(min(df['n']),max(df['n'])))
-    plt.setp(ax, ylim=(-10,10))
+    plt.setp(ax, xlim=(min(df[x_var]),max(df[x_var])))
+    if plot_type == 'mse':
+        plt.setp(ax, ylim=(0,np.median(df['bias']**2 + df['var'])))
+    else:
+        plt.setp(ax, ylim=(-3,3))
     f.suptitle(model)
     f.set_figheight(10)
     f.set_figwidth(10)
     f.supxlabel(x_var)
+    f.supylabel(plot_type)
 
     for j in range(ncol):
         ax[0,j].set_title("{}={}".format(col_var,cols[j]))
         for i in range(nrow):
             ax[i,0].set_ylabel("{}={}".format(row_var,rows[i]))
+            ax[i,j].grid()
             for x in df["estimator"].unique():
                 sub_df = df[(df[row_var] == rows[i]) & (df[col_var] == cols[j]) & (df["estimator"] == x)]
                 if plot_type == "mse":
@@ -37,6 +44,25 @@ def plot_robustness(df, col_var, row_var, model, x_var, plot_type="bias", save_p
                     ax[i,j].plot(sub_df[x_var], sub_df['bias'], label=x)
                     sd = np.sqrt(sub_df['var'])
                     ax[i,j].fill_between(sub_df[x_var],  sub_df['bias']-sd, sub_df['bias']+sd, alpha=0.15)
+                
     ax[0,0].legend()
     plt.show()
     f.savefig(save_path)
+
+
+
+if __name__ == '__main__':
+    file_name = 'sbm_robustness.pkl'
+    with open(file_name, 'rb') as file:
+        data = pickle.load(file)
+
+    df = pd.DataFrame(data)
+    model = "Ugander-Yin Model"
+    plot_type = "bias"
+    save_path = "robustness.png"
+
+    col_var = "beta"
+    row_var = "q"
+    x_var = "n"
+    
+    plot_robustness(df, col_var, row_var, model, x_var, plot_type, save_path)
